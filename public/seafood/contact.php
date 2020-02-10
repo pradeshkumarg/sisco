55ad7e67361e5570c92dcd88e55b2b8f6f6102b1
Pradesh Kumar <pradeshkumar1@outlook.com>
2020-02-11 01:03:25 +0530

    Changing Receiver Email Address

--- public/seafood/contact.php
+++ public/seafood/contact.php
@@ -9,7 +9,7 @@ if(!empty($_POST["send"])) {
 	$email = $_POST["userEmail"];
 
 
-	$to = "pradesh@winacore.com"; // this is your Email address
+	$to = "ashok@siscofoods.com"; // this is your Email address
     $from = $_POST['userEmail']; // this is the sender's Email address
     $subject = "Downloaded the catalogue";
 	$message="Details about the downloaded customer" . "\n\n" . "First Name : " . $_POST['first_name'] . "\n\n" . "Name : " . $_POST['last_name'] . "\n\n" . "Company Name : " . $_POST['company'] . "\n\n" . "Email ID : " . $_POST['userEmail'];
